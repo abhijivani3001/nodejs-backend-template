001254e929730094a0ef3b1fb0e1202de4a2c85d
abhijivani3001 <abhijivani3001@gmail.com>
2024-09-01 01:13:01 +0530

    feat: Rate limit setup

--- src/constant/responseMessage.ts
+++ src/constant/responseMessage.ts
@@ -2,4 +2,5 @@ export default {
   SUCCESS: `The operation has been successfully completed.`,
   SOMETHING_WENT_WRONG: `Something went wrong!`,
   NOT_FOUND: (entity: string) => `The ${entity} was not found.`,
+  TOO_MANY_REQUESTS: `Too many requests! Please try again later.`,
 };
--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import apiController from '../controller/apiController';
+import rateLimiter from '../middleware/rateLimiter';
 
 const router = Router();
 
-router.route('/self').get(apiController.self);
+router.route('/self').get(rateLimiter, apiController.self); // rateLimiter middleware is added for this route
 router.route('/health').get(apiController.health);
 
 export default router;
