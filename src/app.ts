17bb8d28f209abe28c822f1372792e82977fc7f0
abhijivani3001 <abhijivani3001@gmail.com>
2024-09-01 00:50:07 +0530

    feat: Application health endpoints and helmet setup

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import router from './router/apiRouter';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import responseMessage from './constant/responseMessage';
 import httpError from './util/httpError';
+import helmet from 'helmet';
 
 const app: Application = express();
 
 // middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
